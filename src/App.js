3ce6223cbeb153c6916c8947eb5e2a90ae69d704
daichigoshima <100004813+daichigoshima@users.noreply.github.com>
2023-08-14 15:47:24 +0900

    mod

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2> Hello React app with AWS test</h2>
+        <h2> Hello React app with AWS tet</h2>
       </header>
     </div>
   );
