b9aa1423f6fb7b7f963a7159eff56f28d61f2151
daichigoshima <100004813+daichigoshima@users.noreply.github.com>
2023-08-15 17:03:51 +0900

    mod

--- src/App.js
+++ src/App.js
@@ -7,23 +7,37 @@ import "@aws-amplify/ui-react/styles.css";
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
-function App({signOut, user}) {
+// function App({signOut, user}) {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h2> Hello React app with AWS test</h2>
+//         {/* {user ? (
+//           <>
+//             <h3>私は権限を持っています:{user.username}</h3>
+//             <button onClick={signOut}>サインアウト</button>
+//           </>
+//         ) : (
+//           <h3>権限がありませんa</h3>
+//         )} */}
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default withAuthenticator(App);
+
+
+function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2> Hello React app with AWS tet</h2>
-        {/* {user ? (
-          <>
-            <h3>私は権限を持っています:{user.username}</h3>
-            <button onClick={signOut}>サインアウト</button>
-          </>
-        ) : (
-          <h3>権限がありませんa</h3>
-        )} */}
+        <h2> Hello React app with AWS tests</h2>
       </header>
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
